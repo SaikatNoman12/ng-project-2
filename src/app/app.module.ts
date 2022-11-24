b237bad691bf6517a353df68b42227dbb1b639e7
saikatnoman12 <noommaa240@gmail.com>
2022-11-24 23:02:03 +0600

    create nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeaderItemComponent } from './header/header-item/header-item.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ErrorComponent } from './error/error.component';
+
+const router: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutUsComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'contact', component: ContactUsComponent },
+  { path: '**', component: ErrorComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderItemComponent,
+    HomeComponent,
+    AboutUsComponent,
+    GalleryComponent,
+    ProductsComponent,
+    ContactUsComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(router)
   ],
   providers: [],
   bootstrap: [AppComponent]
