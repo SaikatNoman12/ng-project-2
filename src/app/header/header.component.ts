9d41132bf12664b90bc42b6ca3d68cf0c63c0f6f
saikatnoman12 <noommaa240@gmail.com>
2022-11-25 12:15:50 +0600

    add home, about, contact static design

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  dataStyle: object = {
+    'margin-right': '6px',
+    'color': 'rgb(13, 110, 253)'
+  };
+
+  goBack: boolean = false;
+  headerNav: boolean = true;
+  headerLogin:boolean = true;
+  headerLogInBack: boolean = false;
+  headerContactDetails:boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
